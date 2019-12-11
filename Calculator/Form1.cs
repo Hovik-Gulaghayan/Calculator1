3b297947b384ffb9eb9fa155d7ffc293c0bee1c5
Hovik-Gulaghayan <hgulaghayan@gmail.com>
2019-12-12 01:44:15 +0400

    Importing project

---
+++ Calculator/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Calculator
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //GraphicsPath path = new GraphicsPath();
+            //path.AddEllipse(-1, -1, this.Width, this.Height);
+            //Region region = new Region(path);
+            //this.Region = region;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calc_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private class GraphicsPath
+        {
+            internal void AddEllipse(int v1, int v2, int width, int height)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
